dfc2ac39aa26c8f0ee2c86fd0af3ab3256fe6b53
Ram Grover <ramgrover.rg@gmail.com>
2024-02-10 15:10:52 -0500

    intitial commit

---
+++ add_two_numbers.py
@@ -0,0 +1,6 @@
+#print("What's the first number?");
+b=input("What's the first number? ");
+#print("What's the second number?");
+c=input("What's the second number? ");
+#a=int(b)+int(c);
+print("The sum of two numbers is",(int(b)+int(c)));
